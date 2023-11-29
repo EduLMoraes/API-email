a7ab1f7212aa0ec01cee0ba44fc8e48a4b0643e1
EduardoMoreaes <17eduardo05@gmail.com>
2023-11-29 01:51:51 -0300

    env port

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@ use actix_web::{HttpServer, App};
 mod router;
 use router::config;
 use std::error::Error;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn Error>>{
+
+    let port = env::var("PORT").unwrap_or_else(|_| "8000".to_string());
+
     HttpServer::new(|| {
             App::new()
             .configure(config)
         }
     )
-    .bind(("0.0.0.0", 8000))?
+    .bind(("0.0.0.0", port.parse().expect("Erro na porta")))?
     .run()
     .await?;
 
